9c18bb63011c37907337dec87897770d40cb61ac
data-miner00 <data-miner00@users.noreply.github.com>
2024-06-16 14:05:17 +0800

    feat: Use time tag to encapsulate publish date

--- components/ArticleFeed.vue
+++ components/ArticleFeed.vue
@@ -17,8 +17,13 @@ const friendlyPublishedDate = computed(() =>
 <template>
   <article>
     <NuxtLink class="block mb-16" :to="url">
-      <p class="text-center text-xs text-gray-400 mb-1 tracking-wide">
-        {{ friendlyPublishedDate }}
+      <p
+        :title="$t('shared.publishedAt', { date: friendlyPublishedDate })"
+        class="text-center text-xs text-gray-400 mb-1 tracking-wide"
+      >
+        <time :datetime="publishedAt.toDateString()">
+          {{ friendlyPublishedDate }}
+        </time>
       </p>
       <h1 class="text-2xl font-bold text-center uppercase mb-8">{{ title }}</h1>
 
