28aeb71a8ec769ac14a3d49c126e4a2b32318783
data-miner00 <data-miner00@users.noreply.github.com>
2024-07-16 13:15:26 +0800

    fix: Remove presentation role

--- components/ArticleFeed.vue
+++ components/ArticleFeed.vue
@@ -46,7 +46,6 @@ const friendlyPublishedDate = computed(() =>
       </div>
 
       <button
-        role="presentation"
         class="block mx-auto px-3 py-2 uppercase border border-solid rounded border-gray-500 text-sm tracking-wide hover:border-green-500 hover:bg-green-500 hover:text-white transition-colors"
       >
         {{ $t("homePage.article-feed.read-more") }}
